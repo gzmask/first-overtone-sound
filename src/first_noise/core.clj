01801798f1a22243187935b63676a7329386b843
gzmask <gzmask@barb.(none)>
2012-08-27 23:20:05 -0600

    change headers

--- src/first_noise/core.clj
+++ src/first_noise/core.clj
@@ -70,44 +70,10 @@
 ;(trem)
 ;(stop)
 
-(definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
-  (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
+(definst sin-wave [freq 440 attack 0.01 decay 0.6 sustain 0.4 release 0.1 vol 0.4] 
+  (* (env-gen (perc 0 decay) 1 1 0 1 FREE)
      (sin-osc freq)
      vol))
+(odoc env-gen)
+
 (sin-wave)
-
-;sc-one
-(comment
-  (demo 10 (sin-osc (+ 1000 (* 600 (lf-noise0:kr 12))) 0.3))
-  (odoc lf-noise0)
-
-  (demo 10 (rlpf (dust [12 15]) (+ 1600 (* 1500 (lf-noise1 [1/3, 1/4]))) 0.02 ))
-
-  (demo 10 (let [sines 5
-                speed 6]
-             (* (mix
-                 (map #(pan2 (* (sin-osc (* % 100))
-                                (max 0 (+ (lf-noise1:kr speed) (line:kr 1 -1 30))))
-                             (- (clojure.core/rand 2) 1))
-                      (range sines)))
-                (/ 1 sines))))
-
-  (demo 10 (let [noise (lf-noise1 3)
-                 saws  (mul-add (lf-saw [5 5.123]) 3 80)
-                 freq  (midicps (mul-add noise 24 saws))
-                 src   (* 0.4 (sin-osc freq))]
-             (comb-n src 1 0.3 2)))
-
-  (demo (* 0.5 (sin-osc 100 (* 10 (sin-osc 500 0)))))
-
-  (demo (* 0.5 (pm-osc 100 500 10 0)))
-
-  (demo 10 (pm-osc 440 (mouse-y:kr 1 550) (mouse-x:kr 1 15)))
-
-  (demo 10
-        (let [trigger       (line:kr :start 1, :end 20, :dur 60)
-              freq          (t-rand:kr :lo 100, :hi 1000, :trig (impulse:kr trigger))
-              num-harmonics (t-rand:kr :lo 1,   :hi 10,   :trig (impulse:kr trigger))
-              amp           (linen:kr :gate (impulse:kr trigger) :attackTime 0, :susLevel 0.5, :releaseTime (/ 1 trigger))]
-          (* amp (blip freq num-harmonics))))
-)
